f415fc2f7f2b266cab78b2af665c1df565e945de
sao <c_pasmoi@outlook.com>
2024-04-15 16:56:59 +0200

    use ngIf and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-  `
+  templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined; 
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const pokemon : Pokemon | undefined= this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if(pokemon) {
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon?.name}`);
+      this.pokemonSelected = pokemon
+    } else {
+      console.log(`Vous avez cliqué sur un pokémon qui n'existe pas`);
+      this.pokemonSelected = pokemon
+    }
+
+  }
 }
