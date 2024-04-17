3594e6bc8af7dcaf1301d2b2a8eb337ad7014267
sao <c_pasmoi@outlook.com>
2024-04-17 12:47:52 +0200

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BorderCardDirective, PokemonTypeColorPipe],
   templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit{
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined; 
-
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon : Pokemon | undefined= this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-    if(pokemon) {
-      console.log(`Vous avez cliqué sur le pokémon ${pokemon?.name}`);
-      this.pokemonSelected = pokemon
-    } else {
-      console.log(`Vous avez cliqué sur un pokémon qui n'existe pas`);
-      this.pokemonSelected = pokemon
-    }
-
-  }
-}
+export class AppComponent {}
