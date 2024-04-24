c66497c2fa51228bf90a11e19e14788a629ac84a
sao <c_pasmoi@outlook.com>
2024-04-24 13:41:02 +0200

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { DetailPokemonComponent } from './pokemon/detail-pokemon/detail-pokemon.
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EditPokemonComponent } from './pokemon/edit-pokemon/edit-pokemon.component';
 import { AddPokemonComponent } from './pokemon/add-pokemon/add-pokemon.component';
+import { authGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: 'edit/pokemon/:id', component: EditPokemonComponent},
-    { path: 'pokemon/add', component: AddPokemonComponent},
-    { path: 'pokemons', component: ListPokemonComponent},
-    { path: 'pokemon/:id', component: DetailPokemonComponent},
-    { path: '', redirectTo: 'pokemons', pathMatch: 'full'},
+    { path: 'edit/pokemon/:id', component: EditPokemonComponent, canActivate: [authGuard]},
+    { path: 'pokemon/add', component: AddPokemonComponent, canActivate: [authGuard]},
+    { path: 'pokemons', component: ListPokemonComponent, canActivate: [authGuard]},
+    { path: 'pokemon/:id', component: DetailPokemonComponent, canActivate: [authGuard]},
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent},
     { path: '**', component: PageNotFoundComponent}
 ];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { routes } from '../app.routes';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styles: ``
+})
+export class LoginComponent implements OnInit {
+  message: string = "Vous êtes déconnecté. (pikachu/pikachu)";
+  name: string; 
+  password: string;
+  auth: AuthService; 
+
+  constructor(
+    private authService: AuthService,
+    private route: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.auth = this.authService;
+  }
+
+  setMessage() {
+    if(this.auth.isLoggedIn) {
+      this.message = 'Vous êtes connecté' 
+    } else {
+      this.message = 'Identifiant ou mot de passe incorrect'
+    }
+  }
+
+  login() {
+    this.message = "Tentative de connexion en cours..."
+    this.auth.login(this.name, this.password)
+      .subscribe((isLoggedIn: boolean) => {
+        this.setMessage();
+        if(isLoggedIn) {
+          this.route.navigate(['/pokemons']);
+        } else {
+          this.password = '';
+          this.route.navigate(['/login']);
+        }
+      })
+  }
+
+  logout() {
+    this.auth.logout();
+    this.message = 'vous êtes déconnecté'
+  }
+}
